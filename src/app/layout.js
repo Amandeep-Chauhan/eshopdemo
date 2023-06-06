c6e23815c2791cfebe60da14198cc4bb42b33cb5
amandeep2508 <amandeep.chauhan@cogoport.com>
2023-06-06 23:47:55 +0530

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import NavBar from '@/components/NavBar'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ContextProvider } from '@/context/AppContext' 
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ContextProvider>
+            <NavBar />
+            {children}
+        </ContextProvider>
+      </body>
     </html>
   )
 }
